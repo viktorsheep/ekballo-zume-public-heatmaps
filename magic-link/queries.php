7bca2079570ce64490e5389c8137c9f906471c69
Chris Chasm <chris@chasm.solutions>
2021-06-04 11:52:01 -0600

    Updated datatable

--- magic-link/queries.php
+++ magic-link/queries.php
@@ -22,9 +22,9 @@ class Zume_Public_Heatmap_Queries {
 
             SELECT
             lg1.grid_id, lg1.population, lg1.country_code
-            FROM location_grid lg1
+            FROM $wpdb->dt_location_grid lg1
             WHERE lg1.level = 0
-			AND lg1.grid_id NOT IN ( SELECT lg11.admin0_grid_id FROM location_grid lg11 WHERE lg11.level = 1 AND lg11.admin0_grid_id = lg1.grid_id )
+			AND lg1.grid_id NOT IN ( SELECT lg11.admin0_grid_id FROM $wpdb->dt_location_grid lg11 WHERE lg11.level = 1 AND lg11.admin0_grid_id = lg1.grid_id )
  			#'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
             AND lg1.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
             #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
@@ -37,9 +37,9 @@ class Zume_Public_Heatmap_Queries {
             --
             SELECT
             lg2.grid_id, lg2.population, lg2.country_code
-            FROM location_grid lg2
+            FROM $wpdb->dt_location_grid lg2
             WHERE lg2.level = 1
-			AND lg2.grid_id NOT IN ( SELECT lg22.admin1_grid_id FROM location_grid lg22 WHERE lg22.level = 2 AND lg22.admin1_grid_id = lg2.grid_id )
+			AND lg2.grid_id NOT IN ( SELECT lg22.admin1_grid_id FROM $wpdb->dt_location_grid lg22 WHERE lg22.level = 2 AND lg22.admin1_grid_id = lg2.grid_id )
              #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
             AND lg2.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
             #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
@@ -51,7 +51,7 @@ class Zume_Public_Heatmap_Queries {
             --
 			SELECT
             lg3.grid_id, lg3.population,  lg3.country_code
-            FROM location_grid lg3
+            FROM $wpdb->dt_location_grid lg3
             WHERE lg3.level = 2
             #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
             AND lg3.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
@@ -64,7 +64,7 @@ class Zume_Public_Heatmap_Queries {
             --
             SELECT
             lg4.grid_id, lg4.population,  lg4.country_code
-            FROM location_grid lg4
+            FROM $wpdb->dt_location_grid lg4
             WHERE lg4.level = 1
             #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
             AND lg4.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
@@ -78,7 +78,7 @@ class Zume_Public_Heatmap_Queries {
             --
             SELECT
             lg5.grid_id, lg5.population, lg5.country_code
-            FROM location_grid as lg5
+            FROM $wpdb->dt_location_grid as lg5
             WHERE
             lg5.level = 3
             #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
