e5815e9d16e9365ddfb80b9930dbacc4e8f87cd9
Chris Chasm <chris@chasm.solutions>
2021-12-14 16:26:11 -0700

    Added list fields

--- magic/custom-tile.php
+++ magic/custom-tile.php
@@ -19,96 +19,8 @@ class Zume_Public_Portal_Fields {
     } // End instance()
 
     public function __construct() {
-//        add_filter( 'dt_details_additional_tiles', [ $this, 'dt_details_additional_tiles' ], 10, 2 );
-//        add_action( 'dt_details_additional_section', [ $this, 'dt_details_additional_section' ], 30, 2 );
-//        add_filter( 'dt_settings_apps_list', [ $this, 'dt_settings_apps_list' ], 10, 1 );
         add_filter( 'dt_custom_fields_settings', [ $this, 'dt_custom_fields_settings' ], 50, 2 );
-    }
-
-    public function dt_details_additional_tiles( $tiles, $post_type = "" ) {
-        if ( $post_type === 'contacts' && ! isset( $tiles["apps"] ) ){
-            $tiles["apps"] = [
-                "label" => __( "Apps", 'disciple_tools' ),
-                "description" => __( "Apps available on this record.", 'disciple_tools' )
-            ];
-        }
-        return $tiles;
-    }
-
-    public function dt_details_additional_section( $section, $post_type ) {
-
-
-        if ( $section === "apps" && $post_type === "contacts" ) {
-            $record = DT_Posts::get_post( $post_type, get_the_ID() );
-            if ( isset( $record[$this->meta_key] )) {
-                $key = $record[$this->meta_key];
-            } else {
-                $key = dt_create_unique_key();
-                update_post_meta( get_the_ID(), $this->meta_key, $key );
-            }
-            $link = DT_Magic_URL::get_link_url( $this->root, $this->type, $key )
-            ?>
-            <div class="section-subheader"><?php echo esc_html( $this->page_title ) ?></div>
-            <div class="section-app-links <?php echo esc_attr( $this->meta_key ); ?>">
-                <a type="button" class="empty-select-button select-button small button view"><img class="dt-icon" src="<?php echo get_template_directory_uri() . '/dt-assets/images/visibility.svg' ?>" /></a>
-                <a type="button" class="empty-select-button select-button small button copy_to_clipboard" data-value="<?php echo esc_html( $link ); ?>"><img class="dt-icon" src="<?php echo get_template_directory_uri() . '/dt-assets/images/duplicate.svg' ?>" /></a>
-                <a type="button" class="empty-select-button select-button small button send"><img class="dt-icon" src="<?php echo get_template_directory_uri() . '/dt-assets/images/send.svg' ?>" /></a>
-                <a type="button" class="empty-select-button select-button small button qr"><img class="dt-icon" src="<?php echo get_template_directory_uri() . '/dt-assets/images/qrcode-solid.svg' ?>" /></a>
-                <a type="button" class="empty-select-button select-button small button reset"><img class="dt-icon" src="<?php echo get_template_directory_uri() . '/dt-assets/images/undo.svg' ?>" /></a>
-            </div>
-            <script>
-                jQuery(document).ready(function(){
-                    if ( typeof window.app_key === 'undefined' ){
-                        window.app_key = []
-                    }
-                    if ( typeof window.app_url === 'undefined' ){
-                        window.app_url = []
-                    }
-                    window.app_key['<?php echo esc_attr( $this->meta_key ) ?>'] = '<?php echo esc_attr( $key ) ?>'
-                    window.app_url['<?php echo esc_attr( $this->meta_key ) ?>'] = '<?php echo esc_url( site_url() . '/' . $this->root . '/' .$this->type . '/' ) ?>'
-
-                    jQuery('.<?php echo esc_attr( $this->meta_key ); ?>.select-button.button.copy_to_clipboard').data('value', `${window.app_url['<?php echo esc_attr( $this->meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $this->meta_key ) ?>']}`)
-                    jQuery('.section-app-links.<?php echo esc_attr( $this->meta_key ); ?> .view').on('click', function(e){
-                        jQuery('#modal-large-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $this->page_title )  ?></h3><span class="small-text"><?php echo esc_html( $this->page_description ) ?></span><hr>`)
-                        jQuery('#modal-large-content').empty().html(`<iframe src="${window.app_url['<?php echo esc_attr( $this->meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $this->meta_key ) ?>']}" style="width:100%;height: ${window.innerHeight - 170}px;border:1px solid lightgrey;"></iframe>`)
-                        jQuery('#modal-large').foundation('open')
-                    })
-                    jQuery('.section-app-links.<?php echo esc_attr( $this->meta_key ); ?> .send').on('click', function(e){
-                        jQuery('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $this->page_title )  ?></h3><span class="small-text">Send a link via email through the system.</span><hr>`)
-                        jQuery('#modal-small-content').empty().html(`<div class="grid-x"><div class="cell"><input type="text" class="note <?php echo esc_attr( $this->meta_key ); ?>" placeholder="Add a note" /><br><button type="button" class="button <?php echo esc_attr( $this->meta_key ); ?>">Send Email with Link</button></div></div>`)
-                        jQuery('#modal-small').foundation('open')
-                        jQuery('.button.<?php echo esc_attr( $this->meta_key ); ?>').on('click', function(e){
-                            let note = jQuery('.note.<?php echo esc_attr( $this->meta_key ); ?>').val()
-                            makeRequest('POST', window.detailsSettings.post_type + '/email_magic', { root: '<?php echo esc_attr( $this->root ); ?>', type: '<?php echo esc_attr( $this->type ); ?>', magic_key: '<?php echo esc_attr( $this->meta_key ); ?>', note: note, post_ids: [ window.detailsSettings.post_id ] } )
-                                .done( data => {
-                                    jQuery('#modal-small').foundation('close')
-                                })
-                        })
-                    })
-                    jQuery('.section-app-links.<?php echo esc_attr( $this->meta_key ); ?> .qr').on('click', function(e){
-                        jQuery('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $this->page_title )  ?></h3><span class="small-text">QR codes are useful for passing the coaching links to mobile devices.</span><hr>`)
-                        jQuery('#modal-small-content').empty().html(`<div class="grid-x"><div class="cell center"><img src="https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${window.app_url['<?php echo esc_attr( $this->meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $this->meta_key ) ?>']}" style="width: 100%;max-width:400px;" /></div></div>`)
-                        jQuery('#modal-small').foundation('open')
-                    })
-                    jQuery('.section-app-links.<?php echo esc_attr( $this->meta_key ); ?> .reset').on('click', function(e){
-                        jQuery('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $this->page_title )  ?></h3><span class="small-text">Reset the security code. No data is removed. Only access. The previous link will be disabled and another one created.</span><hr>`)
-                        jQuery('#modal-small-content').empty().html(`<button type="button" class="button <?php echo esc_attr( $this->meta_key ); ?> delete-and-reset">Delete and replace the app link</button> <span class="loading-spinner"></span>`)
-                        jQuery('#modal-small').foundation('open')
-                        jQuery('.button.<?php echo esc_attr( $this->meta_key ); ?>.delete-and-reset').on('click', function(e){
-                            jQuery('.button.<?php echo esc_attr( $this->meta_key ); ?>.delete-and-reset').prop('disable', true)
-                            window.API.update_post('<?php echo esc_attr( $post_type ); ?>', <?php echo esc_attr( get_the_ID() ); ?>, { ['<?php echo esc_attr( $this->meta_key ); ?>']: window.sha256(Date.now()) })
-                                .done( newPost => {
-                                    console.log( newPost )
-                                    jQuery('#modal-small').foundation('close')
-                                    window.app_key['<?php echo esc_attr( $this->meta_key ) ?>'] = newPost['<?php echo esc_attr( $this->meta_key ) ?>']
-                                    jQuery('.section-app-links.<?php echo esc_attr( $this->meta_key ); ?> .select-button.button.copy_to_clipboard').data('value', `${window.app_url['<?php echo esc_attr( $this->meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $this->meta_key ) ?>']}`)
-                                })
-                        })
-                    })
-                })
-            </script>
-            <?php
-        }
+        add_filter( "dt_user_list_filters", [ $this, "dt_user_list_filters" ], 10, 2 );
     }
 
     public function dt_custom_fields_settings( array $fields, string $post_type = "" ) {
@@ -222,14 +134,134 @@ class Zume_Public_Portal_Fields {
         return $fields;
     }
 
-    public function dt_settings_apps_list( $apps_list ) {
-        $apps_list[$this->meta_key] = [
-            'key' => $this->meta_key,
-            'url_base' => $this->root. '/'. $this->type,
-            'label' => $this->page_title,
-            'description' => $this->page_description,
-        ];
-        return $apps_list;
+    public function dt_user_list_filters( $filters, $post_type ){
+        if ( $post_type === $this->post_type ){
+
+            $fields = DT_Posts::get_post_field_settings( $post_type );
+            if ( current_user_can( 'dt_all_admin_' . $this->post_type  ) ){
+                $counts = self::get_all_status_types();
+                $status_counts = [];
+                $total_all = 0;
+                foreach ( $counts as $count ){
+                    $total_all += $count["count"];
+                    dt_increment( $status_counts[$count["status"]], $count["count"] );
+                }
+                $filters["tabs"][] = [
+                    "key" => "all",
+                    "label" => __( "Leadership Milestones", 'disciple-tools' ),
+                    "order" => 20
+                ];
+                foreach ( $fields["leader_milestones"]["default"] as $status_key => $status_value ) {
+                    if ( isset( $status_counts[$status_key] ) ){
+                        $filters["filters"][] = [
+                            "ID" => 'all_' . $status_key,
+                            "tab" => 'all',
+                            "name" => $status_value["label"],
+                            "query" => [
+                                'leader_milestones' => [ $status_key ],
+                                'sort' => '-post_date'
+                            ],
+                            "count" => $status_counts[$status_key]
+                        ];
+                    }
+                }
+            }
+            else {
+                $counts = self::get_my_status();
+                /**
+                 * Setup my filters
+                 */
+                $active_counts = [];
+                $status_counts = [];
+                $total_my = 0;
+                foreach ( $counts as $count ){
+                    $total_my += $count["count"];
+                    dt_increment( $status_counts[$count["status"]], $count["count"] );
+                    if ( $count["status"] === "active" ){
+                        dt_increment( $active_counts[$count["status"]], $count["count"] );
+                    }
+                }
+
+                $filters["tabs"][] = [
+                    "key" => "assigned_to_me",
+                    "label" => __( "My Leader Milestones", 'disciple-tools' ),
+                    "order" => 20
+                ];
+                foreach ( $fields["leader_milestones"]["default"] as $status_key => $status_value ) {
+                    if ( isset( $status_counts[$status_key] ) ){
+                        $filters["filters"][] = [
+                            "ID" => 'my_' . $status_key,
+                            "tab" => 'assigned_to_me',
+                            "name" => $status_value["label"],
+                            "query" => [
+                                'assigned_to' => [ 'me' ],
+                                'leader_milestones' => [ $status_key ],
+                                'sort' => '-post_date'
+                            ],
+                            "count" => $status_counts[$status_key]
+                        ];
+                    }
+                }
+            }
+        }
+        return $filters;
+    }
+
+    public function get_my_status(){
+        /**
+         * @todo adjust query to return count for update needed
+         */
+        global $wpdb;
+        $post_type = $this->post_type ;
+        $current_user = get_current_user_id();
+
+        $results = $wpdb->get_results( $wpdb->prepare( "
+            SELECT pm.meta_value as status, count(pm.post_id) as count
+             FROM $wpdb->postmeta pm
+            INNER JOIN $wpdb->posts a ON ( a.ID = pm.post_id AND a.post_type = %s and a.post_status = 'publish' )
+            INNER JOIN $wpdb->postmeta as assigned_to ON pm.post_id=assigned_to.post_id
+                          AND assigned_to.meta_key = 'assigned_to'
+                          AND assigned_to.meta_value = CONCAT( 'user-', %s )
+            WHERE pm.meta_key = 'leader_milestones'
+            GROUP BY pm.meta_value
+        ", $post_type, $current_user ), ARRAY_A);
+
+//        dt_write_log(__METHOD__);
+//        dt_write_log($results);
+
+        return $results;
+    }
+
+    //list page filters function
+    public function get_all_status_types(){
+        /**
+         * @todo adjust query to return count for update needed
+         */
+        global $wpdb;
+        if ( current_user_can( 'dt_all_admin_'.$this->post_type ) ){
+            $results = $wpdb->get_results($wpdb->prepare( "
+                SELECT pm.meta_value as status, count(pm.post_id) as count
+                 FROM $wpdb->postmeta pm
+                INNER JOIN $wpdb->posts a ON ( a.ID = pm.post_id AND a.post_type = %s and a.post_status = 'publish' )
+                WHERE pm.meta_key = 'leader_milestones'
+                GROUP BY pm.meta_value
+            ", $this->post_type  ), ARRAY_A );
+        } else {
+            $results = $wpdb->get_results($wpdb->prepare("
+                SELECT status.meta_value as status, count(pm.post_id) as count
+                FROM $wpdb->postmeta pm
+                INNER JOIN $wpdb->postmeta status ON( status.post_id = pm.post_id AND status.meta_key = 'leader_milestones' )
+                INNER JOIN $wpdb->posts a ON( a.ID = pm.post_id AND a.post_type = %s and a.post_status = 'publish' )
+                LEFT JOIN $wpdb->dt_share AS shares ON ( shares.post_id = a.ID AND shares.user_id = %s )
+                LEFT JOIN $wpdb->postmeta assigned_to ON ( assigned_to.post_id = pm.post_id AND assigned_to.meta_key = 'assigned_to' && assigned_to.meta_value = %s )
+                WHERE ( shares.user_id IS NOT NULL OR assigned_to.meta_value IS NOT NULL )
+                GROUP BY status.meta_value, pm.meta_value
+            ", $this->post_type , get_current_user_id(), 'user-' . get_current_user_id() ), ARRAY_A);
+        }
+//        dt_write_log(__METHOD__);
+//        dt_write_log($results);
+
+        return $results;
     }
 }
 Zume_Public_Portal_Fields::instance();
