d49088b9680139202e04885aea0f828515c66d3e
Saw Nerdar <sheep.blinker@gmail.com>
2022-12-09 18:54:53 +0630

    version update

--- zume-public-heatmaps.php
+++ zume-public-heatmaps.php
@@ -5,7 +5,7 @@
  * Description: This plugin creates the public facing heatmaps that show trainings and churches and are embedded into public websites.
  * Text Domain: ekballo-zume-public-heatmaps
  * Domain Path: /languages
- * Version:  0.4.12
+ * Version:  0.5.13
  * Author URI: https://github.com/viktorsheep
  * GitHub Plugin URI: https://github.com/viktorsheep/ekballo-zume-public-heatmaps
  * Requires at least: 4.7.0
